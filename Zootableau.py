01f0dd55e848905512d096143fad79dc37f4f954
Blind <55689277+BaretSacha@users.noreply.github.com>
2020-03-06 12:56:06 +0100

    Update Zootableau.py

--- Zootableau.py
+++ Zootableau.py
@@ -12,10 +12,10 @@ class animaux:
         self.Quantite_de_nourriture = Quantite_de_nourriture
 
     def __str__(self):
-        return "Cest animal est un " + self.name  + "\nIl y a  " + self.nombres +  self.name + "\nCest animal est un " + self.regime_alimentaire + "\nCest animal a " + self.Nombres_de_pattes + "pattes" "\n Cest animal mange " + self.Quantite_de_nourriture + "grammes de nourritures."
+        return "Cest animal est un " + self.name  + "\nIl y a  " + str(self.nombres) + " " + self.name + "\nCet animal est un " + self.regime_alimentaire + "\nCet animal a " + str(self.Nombres_de_pattes) +" " +  "pattes" "\n Cet animal mange " + str(self.Quantite_de_nourriture) +" " +  "grammes de nourritures."
 
 class animaux_different:
-    def __init__(self,name,nombres,regimes_alimentaiore,Quantite_de_nourriture):
+    def __init__(self,name,nombres,regime_alimentaire,Nombres_de_pattes,Quantite_de_nourriture):
         self.name = name
         self.nombres = nombres
         self.regime_alimentaire = regime_alimentaire
@@ -23,7 +23,7 @@ class animaux_different:
         self.Quantite_de_nourriture = Quantite_de_nourriture
 
     def __str__(self):
-        return "Cest animal est un " + self.name  + "\nIl y a  " + self.nombres ,  self.name + "\nCest animal est un " + self.regime_alimentaire + "\nCest animal a " + self.Nombres_de_pattes + "pattes" "\n Cest animal mange " + self.Quantite_de_nourriture + "grammes de nourritures."
+        return "Cest animal est un " + self.name  + "\nIl y a  " + str(self.nombres) ,  self.name + "\nCet animal est un " + self.regime_alimentaire + "\nCet animal a " + str(self.Nombres_de_pattes) + "pattes" "\n Cet animal mange " + str(self.Quantite_de_nourriture) + "grammes de nourritures."
 
 serpent = animaux_different ('serpent',2,'Carnivore',0,200)
 autruche = animaux_different ('autruche',4,'Omnivire',2,1000)
@@ -60,3 +60,5 @@ pieuvre = animaux_marins ('pieuvre',1,'Carnivore',0,200)
 if __name__ == "__main__":
 
     print(humain)
+    print(mouton)
+    
